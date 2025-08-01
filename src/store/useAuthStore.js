156d57695737b5ed2d42637a2447f465c48183d3
prakash <prakashvadakadu8@gmail.com>
2025-08-01 08:43:49 +0530

    first commit

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -79,7 +79,7 @@ export const useAuthStore = create((set, get) => ({
     const { authUser } = get();
     if (!authUser || get().socket?.connected) return;
 
-    const socket = io(BASE_URL, {
+    const socket = io(CHAT_URL, {
       query: {
         userId: authUser._id,
       },
